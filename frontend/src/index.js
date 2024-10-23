046395c06ae7cbb8ec2000d122093d004b04bc32
Tarun <tarunmalviya8827@gmail.com>
2024-10-23 14:13:10 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+
 import App from './App';
 import './index.css';
 
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+   
       <App />
-    </BrowserRouter>
+ 
   </React.StrictMode>
 );
